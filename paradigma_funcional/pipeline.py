f2b63f81472ded34457d7b8c9f6da9ff11c05a81
nazarenomm <nazarenomm98@gmail.com>
2024-03-10 17:37:26 -0300

    ejercicios

--- paradigma_funcional/pipeline.py
+++
@@ -1,8 +0,0 @@
-import csv
-from collections.abc import Iterator
-
-def generar_lineas(path: str) -> Iterator[list[str]]:
-    with open(path) as f:
-        csv_reader = csv.reader(f)
-        for linea in csv_reader:
-            yield linea
