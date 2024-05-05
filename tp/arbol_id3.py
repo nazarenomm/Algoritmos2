a19d5dd8e9caceaab4565fcf2d570c0ce0db93ed
nazarenomm <nazarenomm98@gmail.com>
2024-05-05 14:22:26 -0300

    arbolID3 sin Data

--- tp/arbol_id3.py
+++ tp/arbol_id3.py
@@ -3,60 +3,47 @@ import pandas as pd
 import numpy as np
 from copy import deepcopy
 
-class Data:
-    def __init__(self, df: pd.DataFrame, target: str) -> None:
-        self.df: pd.DataFrame = df
+class Nodo:
+    def __init__(self, data: pd.DataFrame, target: str) -> None: #data : data de entrenamiento
+        self.atributo: Optional[str] = None # se setea cuando se haga un split
+        self.categoria: Optional[str] = None # lo mismo
+        self.data: pd.DataFrame = data
         self.target: str = target
+        self.si: Optional[ArbolID3] = None
+        self.sd: Optional[ArbolID3] = None
+
+    def split(self, atributo: str, categoria: str) -> None:
+        # nuevo = deepcopy(self) # TODO: copy propio
+        nueva_data_si = self.data[self.data[atributo] == categoria]
+        nueva_data_sd = self.data[self.data[atributo] != categoria]
+        nodo_izq = Nodo(nueva_data_si, self.target)
+        nodo_der = Nodo(nueva_data_sd, self.target)
+        self.sd = ArbolID3(nodo_der)
+        self.si = ArbolID3(nodo_izq)
+        self.atributo = atributo
+        self.categoria = categoria
     
-    def filtrar(self, atributo: str, valor: str, igual: bool = True) -> "Data":
-        if igual:
-            nuevo_df = df[df[atributo] == valor]
-        else:
-            nuevo_df = df[df[atributo] != valor]
-        return Data(nuevo_df, self.target)
-        
     def entropia(self) -> float:
         entropia = 0
-        proporciones = self.df[self.target].value_counts(normalize= True)
-        target_categorias = self.df[self.target].unique()
+        proporciones = self.data[self.target].value_counts(normalize= True)
+        target_categorias = self.data[self.target].unique()
         for c in target_categorias:
             proporcion = proporciones.get(c, 0)
             entropia += proporcion * np.log2(proporcion)
         return -entropia
-
-class Nodo:
-    def __init__(self, data: Data) -> None: #data : data de entrenamiento
-        self.atributo: Optional[str] = None # se setea cuando se haga un split
-        self.categoria: Optional[str] = None# lo mismo
-        self.data: Data = data
-        self.si: Optional[ArbolID3] = None
-        self.sd: Optional[ArbolID3] = None
-
-    def split(self, atributo: str, categoria: str) -> "Nodo":
-        nuevo = deepcopy(self) # hacer copy propio
-        nueva_data_si = self.data.filtrar(atributo, categoria)
-        nueva_data_sd = self.data.filtrar(atributo, categoria, False)
-        nodo_izq = Nodo(nueva_data_si)
-        nodo_der = Nodo(nueva_data_sd)
-        nuevo.sd = ArbolID3(nodo_der)
-        nuevo.si = ArbolID3(nodo_izq)
-        nuevo.atributo = atributo
-        nuevo.categoria = categoria
-        return nuevo
     
     
 class ArbolID3:
     def __init__(self, nodo: Nodo) -> None:
         self.raiz: Nodo = nodo
 
+    # solo para probar metodos
     @staticmethod
     def crear_arbol(df: pd.DataFrame, target: str):
-        data = Data(df, target)
-        nodo = Nodo(data)
+        nodo = Nodo(df, target)
         return ArbolID3(nodo)
 
     def fit(self, df: pd.DataFrame, target: str) -> "ArbolID3": # construye el arbol (?)
-        data = Data(df, target)
         pass
         # decido cual va a ser atributo y su valor por el que splitear y guardo el atributo como raiz
         # hago el split con ese atributo y valor (categoria)
@@ -65,19 +52,20 @@ class ArbolID3:
     # podria ser helper de fit o split
     def information_gain(self, atributo: str, categoria: str) -> float:
         # recopilo informacion necesaria para el calculo
-        entropia_actual = self.raiz.data.entropia()
-        len_actual = len(self.raiz.data.df)
+        entropia_actual = self.raiz.entropia()
+        len_actual = len(self.raiz.data)
 
         information_gain = entropia_actual
         
-        # hago el split por el atributo
-        nodo = self.raiz.split(atributo, categoria)
+        # hago el split:  "atributo = categoria ?"
+        nuevo = deepcopy(self)
+        nuevo.raiz.split(atributo, categoria)
 
         # calculo IG (horrible)
-        entropia_izq = nodo.si.raiz.data.entropia()
-        len_izq = len(nodo.si.raiz.data.df)
-        entropia_der = nodo.sd.raiz.data.entropia()
-        len_der = len(nodo.sd.raiz.data.df)
+        entropia_izq = nuevo.raiz.si.raiz.entropia()
+        len_izq = len(nuevo.raiz.si.raiz.data)
+        entropia_der = nuevo.raiz.sd.raiz.entropia()
+        len_der = len(nuevo.raiz.sd.raiz.data)
 
         return information_gain - ((len_izq/len_actual)*entropia_izq + (len_der/len_actual)*entropia_der)
     
@@ -98,8 +86,14 @@ if __name__ == "__main__":
     df = pd.read_csv("tp/play_tennis.csv")
     print(df.head())
 
-    arbol = ArbolID3.crear_arbol(df, "play")
+    arbol = ArbolID3.crear_arbol(df, target= "play")
 
-    print(f"entropia inicial: {arbol.raiz.data.entropia()}")
+    print(f"entropia inicial: {arbol.raiz.entropia()}")
 
     print(f"information gain de splitear por {"wind = Weak ?"} : {arbol.information_gain("wind", "Weak")}")
+
+    arbol.raiz.split("wind", "Weak")
+
+    print(f"data del subarbol izquierdo luego del split (casos positivos):\n {arbol.raiz.si.raiz.data}")
+    
+    print(f"data del subarbol derecho (casos negativos):\n {arbol.raiz.sd.raiz.data}")
